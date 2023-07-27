83c8b63b1374dbae2720979bc2a9ac7ccb7b8376
lesponns <adamadakadam1903@gmail.com>
2023-07-27 18:54:34 +0300

    Add project files.

---
+++ SmartPro_11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartPro_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //i++ = i+1
+            int repeater;
+
+            Console.WriteLine("Kaç kez tekrarlansın?");
+            Console.Write("==> ");
+            repeater = int.Parse(Console.ReadLine());
+
+            repeater++;
+            for (int i = 1; i < repeater; i++)
+            {
+                Console.WriteLine(i+". Murat");
+            }
+            Console.ReadKey();
+        }
+    }
+}
